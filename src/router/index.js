25cf6e069128ef99189ce72fbbc04de2cbf14bff
Christopher <khseredin@phoenixit.ru>
2022-02-11 18:24:50 +0300

    fixed: router

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,13 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/registration',
   body('email').isEmail(),
-  body('password').isLength({min: 3, max: 32}),
+  body('password').isLength({ min: 3, max: 32 }),
   userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/user', userController.getUser);
+router.get('/user', authMiddleware, userController.getUser);
 router.get('/users', userController.getUsers);
 router.post('/user/update', userController.updateUser);
 
